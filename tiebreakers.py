34f50a26143e453577c03db4fed7b544ab35cf8a
drmoore718 <drmoore718@gmail.com>
2022-10-15 16:20:46 -0700

    Changes for TI11 groups

--- tiebreakers.py
+++ tiebreakers.py
@@ -1,72 +1,115 @@
 data = {
     'a': {
         'nameMap': {
-            'ig': 'Invictus Gaming',
-            'og': 'OG',
             'eg': 'Evil Geniuses',
-            'undying': 'Team Undying',
-            'vp': 'Virtus.pro',
-            't1': 'T1',
-            'alliance': 'Alliance',
-            'aster': 'Team Aster',
-            'tp': 'Thunder Predator',
+            'rng': 'Royal Never Give Up',
+            'og': 'OG',
+            'tl': 'Team Liquid',
+            'gg': 'Gaimin Gladiators',
+            'h': 'Hokori',
+            'lgd': 'PSG.LGD',
+            's': 'Soniqs',
+            'boom': 'BOOM Esports',
+            'bb': 'BetBoom Team',
         },
         'scores': {
-            'ig': 13,
-            'og': 9,
-            'vp': 9,
-            'eg': 8,
-            'undying': 8,
-            't1': 8,
-            'aster': 5,
-            'alliance': 4,
-            'tp': 0,
+            'eg': 6,
+            'rng': 5,
+            'og': 4,
+            'tl': 4,
+            'gg': 3,
+            'h': 3,
+            'lgd': 2,
+            's': 2,
+            'boom': 1,
+            'bb': 0,
         },
         'games': [
-            ['ig',  'og',           ],
-            ['eg',  'undying',      ],
-            ['vp',  'aster',        ],
-            ['t1',  'tp',           ],
+            # ['lgd','og'],
+            # ['boom','gg'],
+            # ['rng','s'],
+            # ['eg','tl'],
+            # ['h','bb'],
+
+            # ['lgd','tl'],
+            # ['s','og'],
+            # ['rng','gg'],
+            # ['eg','h'],
+            # ['boom','bb'],
+
+            # ['eg','bb'],
+            # ['og','h'],
+            # ['s','boom'],
+            # ['tl','gg'],
+            # ['lgd','rng'],
+
+            # ['tl','og'],
+            # ['lgd','boom'],
+            # ['s','bb'],
+            # ['eg','gg'],
+            # ['rng','h'],
+
+            # ['eg','rng'],
+            # ['tl','boom'],
+            # ['lgd','gg'],
+            # ['og','bb'],
+            # ['s','h'],
+
+            ['og','rng'],
+            ['lgd','bb'],
+            ['tl','h'],
+            ['eg','boom'],
+            ['s','gg'],
         ]
     },
     'b': {
         'nameMap': {
-            'lgd': 'PSG.LGD',
-            'vg': 'Vici Gaming',
-            'secret': 'Secret',
-            'bc': 'beastcoast',
-            'elephant': 'Elephant',
+            'aster': 'Team Aster',
+            'secret': 'Team Secret',
             'ts': 'Team Spirit',
+            'ta': 'Thunder Awaken',
+            'tsm': 'TSM',
+            'te': 'Tundra Esports',
+            'bc': 'beastcoast',
+            'ent': 'Entity',
             'fnatic': 'Fnatic',
-            'qc': 'Quincy Crew',
-            'sg': 'SG esports',
+            'talon': 'Talon Esports',
         },
         'scores': {
-            'lgd': 11,
-            'vg': 10,
-            'secret': 8,
-            'bc': 7,
-            'elephant': 6,
-            'ts': 6,
-            'fnatic': 4,
-            'qc': 2,
-            'sg': 2,
+            'aster':    4,
+            'secret':   3,
+            'ts':       3,
+            'ta':       3,
+            'tsm':      3,
+            'te':       3,
+            'bc':       3,
+            'ent':      3,
+            'fnatic':   3,
+            'talon':    1,
         },
         'games': [
-            ['lgd',     'elephant',     ],
-            ['qc',      'sg',           ],
-            ['ts',      'bc',           ],
-            ['secret',  'fnatic',       ],
+            ['talon','ent'],
+            ['secret','aster'],
+            ['ta','tsm'],
+            ['ts','bc'],
+            ['fnatic','te'],
 
-            ['secret',  'vg',           ],
-            ['lgd',     'bc',           ],
-            ['elephant','qc',           ],
-            ['ts',      'sg',           ],
+            ['aster','ts'],
+            ['fnatic','tsm'],
+            ['te','ent'],
+            ['secret','talon'],
+            ['ta','bc'],
         ]
     }
 }
 
+progress = 0
+lastProgress = 0
+
 def resolveGroup(name, groupData):
+    global progress
+    global lastProgress
+
     startScores = groupData['scores']
     games = groupData['games']
     nameMap = groupData['nameMap']
@@ -89,7 +132,7 @@ def resolveGroup(name, groupData):
     for x in games:
         # If odds/probabilities are not provided, use even-odds
         if len(x) == 2:
-            x.extend([0.25, 0.5, 0.25])
+            x.extend([0.25, 0.25, 0.50])
 
         if x[2] >= 1:
             sum = 0
@@ -114,10 +157,13 @@ def resolveGroup(name, groupData):
     tiebreakers = {
         'upper': [[0 for x in range(teamCount)] for y in range(teamCount)],
         'lower': [[0 for x in range(teamCount)] for y in range(teamCount)],
+        'spread': [[0 for x in range(teamCount)] for y in range(teamCount)],
     }
 
     stack = []
 
+    progress = 0
+    lastProgress = 0
     resolveGame(0, startScores, 1, stack, tiebreakers, results, rankings, games, expectedGames)
 
     # for key, value in results.items():
@@ -155,20 +201,39 @@ def resolveGroup(name, groupData):
 
     print()
 
+    hasSpreadTies = False
+    for key, value in rankings.items():
+        if value['spreadTie'] != 0:
+            hasSpreadTies = True
+
     print('## Team placements')
     print()
-    print('Team | Upper Bracket | Upper Bracket Tiebreaker | Lower Bracket | Elimination Tiebreaker | Eliminated')
-    print('----|----|----|----|----|----')
-    for key, value in rankings.items():
-        print('{} | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}%'.format(nameMap[key],
-                value['upper']*100,
-                value['upperTie']*100,
-                value['lower']*100,
-                value['lowerTie']*100,
-                value['out']*100,
-                #value['spreadTie']*100,
+
+    if hasSpreadTies:
+        print('Team | Upper Bracket | Upper Bracket Tiebreaker | Lower Bracket | Elimination Tiebreaker | Spread Tiebreaker | Eliminated')
+        print('----|----|----|----|----|----|----')
+        for key, value in rankings.items():
+            print('{} | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}%'.format(nameMap[key],
+                    value['upper']*100,
+                    value['upperTie']*100,
+                    value['lower']*100,
+                    value['lowerTie']*100,
+                    value['spreadTie']*100,
+                    value['out']*100,
+                )
             )
-        )
+    else:
+        print('Team | Upper Bracket | Upper Bracket Tiebreaker | Lower Bracket | Elimination Tiebreaker | Eliminated')
+        print('----|----|----|----|----|----')
+        for key, value in rankings.items():
+            print('{} | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}% | {:.2f}%'.format(nameMap[key],
+                    value['upper']*100,
+                    value['upperTie']*100,
+                    value['lower']*100,
+                    value['lowerTie']*100,
+                    value['out']*100,
+                )
+            )
 
     print()
 
@@ -186,8 +251,10 @@ def resolveGroup(name, groupData):
                         print()
                         if key == 'upper':
                             print('### Upper Bracket Tiebreakers')
-                        else:
+                        elif key == 'lower':
                             print('### Elimination Tiebreakers')
+                        else:
+                            print('### Spread Tiebreakers')
 
                     if j+1 == 1:
                         ordinal = str(j+1) + 'st'
@@ -203,74 +270,51 @@ def resolveGroup(name, groupData):
     # for key, value in expectedGames.items():
     #     print('* {}: {:.2f} games'.format(teamNameMap[key], value))
 
+    print()
+    print()
 
 def resolveGame(gameNum, scores, probability, stack, tiebreakers, results, rankings, games, expectedGames):
+    global progress
+    global lastProgress
+
     if gameNum >= len(games):
-        place = 0
-        prevPlace = 0
-        prevWins = 0
-        numTied = 1
-        # progress += probability
+        teamCount = len(rankings)
+        progress += probability
 
         # if int(progress*100) > lastProgress:
-        #     #print(int(progress*100))
+        #     print(int(progress*100))
         #     lastProgress = int(progress*100)
 
         what = sorted(scores, key=lambda x: scores[x], reverse=True)
 
-        for key in what:
-
-            wins = scores[key]
-            actualPlace = place
-
-
-            if prevWins == wins:
-                actualPlace = prevPlace
-                numTied += 1
-
-                if place == 8 and numTied + prevPlace > 8 and prevPlace + 1 <= 8:
-                    tiebreakers['lower'][numTied][actualPlace] += probability
-                    for z in range(prevPlace, prevPlace+numTied):
-                        expectedGames[what[z]] += (numTied - 1) * probability
-                    if numTied >= 4:
-                        print('{} way tie for {}th {}'.format(numTied, actualPlace+1, stack))
-            else:
-                if numTied + prevPlace > 4 and prevPlace + 1 <= 4:
-                    tiebreakers['upper'][numTied][prevPlace] += probability
-                    for z in range(prevPlace, prevPlace+numTied):
-                        expectedGames[what[z]] += (numTied - 1) * probability
-                    if numTied >= 4:
-                        print('{} way tie for {}th {}'.format(numTied, prevPlace+1, stack))
-
-
-                prevPlace = place
-                numTied = 1
-
-            results[key][actualPlace] += probability
-            place += 1
-            prevWins = wins
-
         place = 0
-        while place < 9:
+        while place < teamCount:
             startPlace = place
 
             key = what[place]
             wins = scores[key]
 
-            while place+1 < 9 and scores[what[place+1]] == wins:
+            while place+1 < teamCount and scores[what[place+1]] == wins:
                 place += 1
 
+            numTied = place - startPlace + 1
 
             if place <= 3:
                 ranking = 'upper'
             elif startPlace >= 8:
                 ranking = 'out'
-            elif startPlace <= 3 and place == 8:
+            elif startPlace <= 3 and place >= 8:
                 ranking = 'spreadTie'
+                tiebreakers['spread'][numTied][startPlace] += probability
+                # print('{} way tie for {}th {}'.format(numTied, startPlace+1, stack))
             elif startPlace <= 3 and place >= 4:
                 ranking = 'upperTie'
-            elif startPlace >= 4 and place == 8:
+                tiebreakers['upper'][numTied][startPlace] += probability
+                # print('{} way tie for {}th {}'.format(numTied, startPlace+1, stack))
+            elif startPlace >= 4 and place >= 8:
                 ranking = 'lowerTie'
+                tiebreakers['lower'][numTied][startPlace] += probability
+                # print('{} way tie for {}th {}'.format(numTied, startPlace+1, stack))
             else:
                 ranking = 'lower'
 
